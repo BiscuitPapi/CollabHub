db718ef9077a0765b4a39c0e38d334265fea3d98
Fahmi Hafiz <fahmihafiz@Fahmis-MacBook-Pro.local>
2023-12-20 15:05:15 +0800

    Added a trigger to allow users to review (studyhub)

--- assets/php/process_invitationResponse.php
+++ assets/php/process_invitationResponse.php
@@ -1,28 +1,74 @@
 <?php
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);
-  include("connection.php");
-  session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+include("connection.php");
+session_start();
 
-  $studyhub_ID = $_POST['studyHub_ID'];
-  $newStatus = $_POST['status'];
+$studyhub_ID = $_POST['studyHub_ID'];
+$newStatus = $_POST['status'];
 
-  // Perform the necessary database update operation to save the changes
-  $sql = "UPDATE invitation SET status = '$newStatus' WHERE studyHub_ID = '$studyhub_ID' AND user_ID = '" . $_SESSION['user_ID'] . "'";
-  $result = mysqli_query($connection, $sql);
+// Use prepared statement to prevent SQL injection
+$sql = "UPDATE invitation SET status = ? WHERE studyHub_ID = ? AND user_ID = ?";
+$stmt = $connection->prepare($sql);
+$stmt->bind_param("sii", $newStatus, $studyhub_ID, $_SESSION['user_ID']);
+$result = $stmt->execute();
 
-  // If the update is successful, perform the insertion into studyhub_members
-  if ($result && $newStatus == "Accepted") {
-    // Insert into studyhub_members
-    $insert_sql = "INSERT INTO studyhub_members (studyhub_ID, user_ID) VALUES ('$studyhub_ID', '" . $_SESSION['user_ID'] . "')";
-    $insert_result = mysqli_query($connection, $insert_sql);
+if ($result) {
+    // Creation for review purposes
+    $query = "SELECT user_ID FROM studyhub_members WHERE studyhub_ID = ?";
+    $stmt = $connection->prepare($query);
+    $stmt->bind_param("i", $studyhub_ID);
+    $stmt->execute();
 
-    if ($insert_result) {
-      echo "Changes saved successfully and studyhub_members updated!";
+    if ($stmt->error) {
+        echo "Error: " . $stmt->error;
     } else {
-      echo "Changes saved successfully, but failed to update studyhub_members!";
+        $result = $stmt->get_result();
+
+        // Inside the while loop for fetching studyhub_members
+        while ($row = $result->fetch_assoc()) {
+            $reviewee = $row['user_ID'];
+            $reviewer = $_SESSION["user_ID"];
+
+            // Insert feedback record for the original reviewer reviewing the original reviewee
+            $insertQuery1 = "INSERT INTO feedback (reviewer, reviewee) VALUES (?, ?)";
+            $insertStmt1 = $connection->prepare($insertQuery1);
+            $insertStmt1->bind_param("ii", $reviewer, $reviewee);
+            $insertStmt1->execute();
+
+            // Insert feedback record for the original reviewee reviewing the original reviewer
+            $insertQuery2 = "INSERT INTO feedback (reviewer, reviewee) VALUES (?, ?)";
+            $insertStmt2 = $connection->prepare($insertQuery2);
+            $insertStmt2->bind_param("ii", $reviewee, $reviewer);
+            $insertStmt2->execute();
+
+            // Check for errors during feedback insertions
+            if ($insertStmt1->error || $insertStmt2->error) {
+                echo "Error inserting feedback: " . $insertStmt1->error . " | " . $insertStmt2->error;
+            } else {
+                // Insert into studyhub_members
+                $insert_sql = "INSERT INTO studyhub_members (studyhub_ID, user_ID) VALUES (?, ?)";
+                $insert_stmt = $connection->prepare($insert_sql);
+                $insert_stmt->bind_param("is", $studyhub_ID, $_SESSION['user_ID']);
+                $insert_result = $insert_stmt->execute();
+
+                // Provide feedback based on the result of the studyhub_members insertion
+                if ($insert_result) {
+                    echo "Changes saved successfully and studyhub_members updated!";
+                } else {
+                    echo "Changes saved successfully, but failed to update studyhub_members!";
+                }
+            }
+
+            // Close prepared statements for feedback insertions
+            $insertStmt1->close();
+            $insertStmt2->close();
+            $insert_stmt->close();
+        }
+
+        $stmt->close();
     }
-  } else {
+} else {
     echo "Failed to save changes!";
-  }
+}
 ?>
